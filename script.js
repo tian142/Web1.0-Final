8c264ab7f44d45db6f6b35e3c2734351244b801e
Tian Qiu <tian.qiu142@gmail.com>
2020-10-06 10:52:30 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const inputSize = document.querySelector('#input-size');
+const selectFont = document.querySelector('#select-font');
+const inputColor = document.querySelector('#input-color');
+const inputBgColor = document.querySelector('#input-bg-color');
+const input = document.querySelector('#text-input');
+const display = document.querySelector('#display');
+const showSize = document.querySelector('#show-size');
+const showFont = document.querySelector('#show-font');
+const showColor = document.querySelector('#show-color');
+const showBgColor = document.querySelector('#show-bg-color');
+
+inputSize.addEventListener('input', () => {
+  display.style.fontSize = `${inputSize.value}px`;
+  showSize.innerHTML = inputSize.value;
+});
+
+selectFont.addEventListener('change', (event) => {
+  display.style.fontFamily = event.target.value;
+  showFont.innerHTML = event.target.value;
+});
+
+inputColor.addEventListener('input', (event) => {
+  display.style.color = event.target.value;
+  showColor.innerHTML = event.target.value;
+});
+
+inputBgColor.addEventListener('input', (event) => {
+  display.style.backgroundColor = event.target.value;
+  showBgColor.innerHTML = event.target.value;
+});
+
+input.addEventListener('input', (event) => {
+  display.innerText = event.target.value;
+});
